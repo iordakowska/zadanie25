59bbb0c9570c5f5277b8f1a3ac4ccb8b53bb0677
i.ordakowska <i.ordakowska@gmail.com>
2022-11-22 12:26:47 +0100

    rozwiązanie 2 Dodanie zadania: jak ustawić value w select form.html - enum status, category jak dodac pole z datą

--- src/main/java/com/example/zadanie25/MainController.java
+++ src/main/java/com/example/zadanie25/MainController.java
@@ -3,6 +3,7 @@ package com.example.zadanie25;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -19,6 +20,7 @@ public class MainController {
     public String home(Model model) {
         List<Task> allTasks = taskRepository.findAll();
         model.addAttribute("tasks", allTasks);
+        model.addAttribute("taskToAdd", new Task());
         return "home";
     }
 
@@ -34,5 +36,17 @@ public class MainController {
         List<Task> allTasks = taskRepository.findTaskByStatusOrderByDateDesc(Status.READY);
         model.addAttribute("tasks", allTasks);
         return "ready";
+    }
+
+    @GetMapping("form")
+    public String form(Model model) {
+        model.addAttribute("taskToAdd", new Task());
+        return "form";
+    }
+
+    @PostMapping("/add")
+    public String addTask(Task task) {
+        taskRepository.save(task);
+        return "redicect:/";
     }
 }
